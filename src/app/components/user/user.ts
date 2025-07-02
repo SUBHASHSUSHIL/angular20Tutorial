8400475507e51dcbcbe01d2c17b2f8fc66c5beb9
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-03 01:09:22 +0530

    api call in put and delete

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -12,38 +12,81 @@ export class User implements OnInit {
 
   http = inject(HttpClient);
 
-  userList:any[] = [];
-  userObj:any = {
-  "userId": 0,
-  "emailId": "",
-  "password": "",
-  "fullName": "",
-  "mobileNo": ""
-}
+  userList: any[] = [];
+  userObj: any = {
+    "userId": 0,
+    "emailId": "",
+    "password": "",
+    "fullName": "",
+    "mobileNo": ""
+  }
 
   ngOnInit(): void {
     this.getUsers()
   }
 
-  getUsers(){
-    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers").subscribe((res:any) => {
+  getUsers() {
+    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers").subscribe((res: any) => {
       this.userList = res;
     });
   }
 
-  onSaveUser(){
+  onSaveUser() {
     debugger;
     this.http.post("https://api.freeprojectapi.com/api/GoalTracker/register", this.userObj).subscribe({
-      next:(result)=>{
+      next: (result) => {
         debugger;
         alert("User Created Successfully");
         this.getUsers()
       },
-      error:(error)=>{
+      error: (error) => {
         debugger;
         alert("Error -" + error.error)
       }
     })
+  }
+
+  onReset() {
+    this.userObj = {
+      "userId": 0,
+      "emailId": "",
+      "password": "",
+      "fullName": "",
+      "mobileNo": ""
+    };
+  }
+
+  onEdit(item: any) {
+    this.userObj = item;
+  }
+
+  onUpdateUser() {
+    this.userObj.CreatedDate = new Date;
+    this.http.put("https://api.freeprojectapi.com/api/GoalTracker/updateUser?id=" + this.userObj.userId, this.userObj).subscribe({
+      next: () => {
+        alert("User update success");
+        this.getUsers()
+      },
+      error: (error) => {
+        alert("Error -" + error.error)
+      }
+    })
+  }
+
+  onDeleteUser(id: number) {
+    debugger;
+    const isDelet = confirm("Are you sure want to Deleted")
+    if (isDelet) {
+      this.http.delete("https://api.freeprojectapi.com/api/GoalTracker/deleteUserById?id=" + id).subscribe({
+        next: () => {
+          alert("User Deleted Success");
+          this.getUsers()
+        },
+        error: (error) => {
+          alert("Error -" + error.error)
+        }
+      })
+    }
   }
 
 }

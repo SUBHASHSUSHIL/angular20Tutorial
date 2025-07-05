ff73bf96a4df4e1bda41741c7cd208e7654dea59
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-05 19:15:47 +0530

    resource and rxresource example

--- src/app/components/resource-api/resource-api.ts
+++ src/app/components/resource-api/resource-api.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-resource-api',
@@ -8,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class ResourceApi {
 
+  http = inject(HttpClient);
+
+  userData = resource({
+    loader: () => {
+      return fetch("https://jsonplaceholder.typicode.com/users").then(result => result.json())
+    }
+  })
+
+  userList = rxResource({
+    stream: () => this.http.get<any[]>("https://jsonplaceholder.typicode.com/users")
+  })
+
+  constructor() {
+    setTimeout(() => {
+      this.userData.reload()
+    }, 6000)
+  }
 }
